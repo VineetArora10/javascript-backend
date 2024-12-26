6516eea4e7d78a17b43a46f48c5ed598f941c038
VineetArora10 <vineetarora010@gmail.com>
2024-12-26 13:01:02 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,59 +1,25 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
+
+const DB_NAME = require('./constants');
+
 const app = express();
 const port = process.env.PORT;
 
-const jsonData = {
-    'name': 'John Doe',
-    'age': 30,
-    'city': 'Brussels',
-    'profession': 'Software Developer'
-};
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on('error', (error)=>{
+            console.log('ERROR: ', error);
+            throw error;
+        });
 
-app.get('/', (req, res) => {
-    res.send('Hello World :)');
-});
-
-app.get('/api/login', (req, res) => {
-    res.send('<h3>Please login at Lake login portal</h3>');
-});
-
-app.get('/api/json', (req, res) => {
-    res.json(jsonData);
-});
-
-app.get('/api/cars', (req, res) => {
-    const cars = [
-        {
-            id: 1,
-            model: 'Mercedes GWagon G63 AMG',
-            brand: 'Mercedes'
-        },
-        {
-            id: 2,
-            model: 'Mercedes Maybach S650',
-            brand: 'Mercedes'
-        },
-        {
-            id: 3,
-            model: 'Toyota Land Cruiser',
-            brand: 'Toyota'
-        },
-        {
-            id: 4,
-            model: 'Toyota Fortuner Legender',
-            brand: 'Toyota'
-        },
-        {
-            id: 5,
-            model: 'Land Rover Range Rover Autobiography',
-            brand: 'Range Rover'
-        }
-    ];
-
-    res.send(cars);
-});
-
-app.listen(port, () => {
-    console.log(`Backend app listening on port ${port}`);
-});
+        app.listen(port, () => {
+            console.log(`Backend app listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Error: ", error);
+        throw error;
+    }
+})();
